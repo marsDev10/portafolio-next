0fd3e2c7d371ac0e8b6048a89877e71293bbbfc2
marsDev <themarswtf@outlook.es>
2025-11-21 14:59:21 -0600

    feat: Created new script export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: 'portafolio-next',
+  assetPrefix: 'portafolio-next'
 };
 
 export default nextConfig;
